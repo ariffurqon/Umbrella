94e4bd31e163c2ec1057c98c4aabfd821e7a0a5e
ariffurqon <arif.furqon@gmail.com>
2015-12-20 14:39:09 -0800

    added stateful annotations

--- index.ios.js
+++ index.ios.js
@@ -1,7 +1,6 @@
-'use strict';
+'use strict'
 
 var React = require('react-native');
-
 var {
   AppRegistry,
   MapView,
@@ -10,22 +9,32 @@ var {
 } = React;
 
 var Umbrella = React.createClass({
-  render: function(){
-    var pins = [{
-      latitute: 37,
-      longitude: -95
-    }];
-    
-    return <MapView 
-        onRegionChangeComplete={this.onRegionChangeComplete}
-        style={styles.map}></MapView>
+  getInitialState: function() {
+    return {
+      pin: {
+        latitude: 0,
+        longitude: 0
+      }
+    };
+  },
+  render: function() {
+    return <MapView
+      annotations={[this.state.pin]}
+      onRegionChangeComplete={this.onRegionChangeComplete}
+      style={styles.map}>
+    </MapView>
   },
   onRegionChangeComplete: function(region) {
-      console.log(region);
-  }   
+    this.setState({
+      pin: {
+        longitude: region.longitude,
+        latitude: region.latitude
+      }
+    });
+  }
 });
 
-var styles = StyleSheet.create ({
+var styles = StyleSheet.create({
   map: {
     flex: 1
   }
