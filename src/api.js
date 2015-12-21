6c878c563ffd03b5baf235519339afcee3e85722
ariffurqon <arif.furqon@gmail.com>
2015-12-20 18:56:27 -0800

    added api

---
+++ src/api.js
@@ -0,0 +1,22 @@
+var _ = require('lodash');
+var rootUrl = 'http://api.openweathermap.org/data/2.5/weather?APPID=a28addbd1088ee7ca59fba773595d76d';
+
+var kelvinToF = function(kelvin) {
+  return Math.round((kelvin - 273.15) * 1.8 + 32) + ' ˚F'
+};
+
+module.exports = function(latitude, longitude) {
+  var url = `${rootUrl}&lat=${latitude}&lon=${longitude}`;
+
+  return fetch(url)
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(json){
+      return {
+        city: json.name,
+        temperature: kelvinToF(json.main.temp),
+        description: _.capitalize(json.weather[0].description)
+      }
+    });
+}
